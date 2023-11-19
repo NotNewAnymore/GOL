32e6d367c13f132b0b7c5986f70bc1712f160494
NotNewAnymore <Henryfleischer01@gmail.com>
2023-11-18 23:59:55 -0800

    Added more worlds, improved visuals
    Added Testworld 3, Herschel, and Acorn, also made live cells use different char.

--- main.rb
+++ main.rb
@@ -3,10 +3,12 @@ require_relative "worldbuffer"
 currentFrame = Worldbuffer.new
 currentFrame.printworld
 sleep 1
-# currentFrame.randomizeworld
-currentFrame.testworld
+currentFrame.randomizeworld
+# currentFrame.testworld
 # currentFrame.testworld2
-
+# currentFrame.testworld3
+# currentFrame.Herschel
+# currentFrame.Acorn
 1000.times{
   puts ""
   # currentFrame.printadjacent
